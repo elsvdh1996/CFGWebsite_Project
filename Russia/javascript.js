7900b42ad2b9b5f272d15fbf4ec73e1a457752b1
munyinchong <56838757+munyinchong@users.noreply.github.com>
2019-12-08 20:20:58 +0000

    Final edits of Russia

--- Russia/javascript.js
+++ Russia/javascript.js
@@ -1,5 +1,7 @@
-$('.submit').click(function(event) { // ALERT WHEN THE BUTTON IS CLICKED
-  alert('Your comment has been submitted');
-})
+$(document).ready(function() { //LISTENING FOR WHEN IT IS READY
+
+  $('.submit').click(function(event) { // ALERT WHEN THE BUTTON IS CLICKED
+    alert('Your comment has been submitted');
+  })
 
 });
